d79e3d58610b3ff9e08a19188b4dc3ac71051f6b
mardj258 <begin258@mail.ru>
2016-03-09 21:19:19 +0300

    Create State.h

---
+++ core/State.h
@@ -0,0 +1,38 @@
+#pragma once
+#pragma once
+
+#include <QString>
+#include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QUrl>
+#include <QDateTime>
+
+/*!
+*  \brief
+*  \details
+*  \author
+*  \version
+*  \date		9 марта 2016
+*  \pre
+*  \bug
+*  \warning
+*  \copyright
+*/
+
+class State {
+private:
+	int m_state_id;
+	QString m_state_name;
+
+public:
+	QString state_name();
+	State(QString state_name);
+	State(int id);
+	~State();
+	int state_id();
+	bool insertIntoDatabase();
+	static bool createTable();
+	static bool completeTable();
+
+};
