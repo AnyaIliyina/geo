0ab7a0a5b22d48b2bedad2bd6e422628e0d703a9
mardj258 <begin258@mail.ru>
2016-03-09 21:52:34 +0300

    Ань, посмотри completeTable, видно там что-то надо с coded сделать. И просто посмотри, может я совсем не то делаю.. ну когда сможешь, конечно :) с днем рождения, не обращай внимания на мои сообщения пока

--- core/State.cpp
+++ core/State.cpp
@@ -3,8 +3,98 @@
 #include <QSqlTableModel>
 #include <QSqlRecord>
 #include "Database.h"
+#include <QTextCodec>
 
 /*!
 \file
 \brief  
 */
+
+QString State::state_name()
+{
+	return m_state_name;
+}
+
+State::State(QString state_name)
+{
+	m_state_name = state_name;
+	m_state_id = 0;
+}
+
+State::State(int id)
+{
+	Database::open();
+	QSqlTableModel model;
+	model.setTable("states");
+	const QString filter = QString("state_id == %1");
+	model.setFilter(filter);
+	model.select();
+	QString state_name = model.record(0).value("state_name").toString();
+	Database::close();
+
+	m_state_id = id;
+	m_state_name= state_name;
+}
+
+State::~State()
+{
+}
+
+int State::state_id()
+{
+	return m_state_id;
+}
+
+bool State::insertIntoDatabase()
+{
+	Database::open();
+	QSqlQuery query;
+	query.prepare("INSERT INTO states(state_name)\
+	VALUES (?)");
+	query.addBindValue(m_state_name);
+	if (!query.exec()) {
+		qDebug() << "State::insertIntoDatabase(): error inserting into Table states";
+		qDebug() << query.lastError().text();
+		Database::close();
+		return false;
+	}
+	Database::close();
+	return true;
+}
+
+bool State::createTable()
+{
+	Database::open();
+	QSqlQuery query;
+	if (!query.exec("CREATE TABLE IF NOT EXISTS states (\
+		state_id INTEGER PRIMARY KEY AUTOINCREMENT, \
+		state_name TEXT UNIQUE NOT NULL\
+		)"
+		))
+	{
+		qDebug() << "error creating states Table in database";
+		qDebug() << query.lastError().text();
+		Database::close();
+		return false;
+	}
+	Database::close();
+	return true;
+}
+
+QString State::coded(QByteArray encodedStr) // метод для получения строки в кодировке Unicode 
+{ // из QByteArray с кодировкой Windows-1251 
+	QTextCodec *codec = QTextCodec::codecForName("Windows-1251");
+	// QTextCodec *codec2 = QTextCodec::codecForName("UTF-8"); 
+	QString const string = codec->toUnicode(encodedStr);
+	return string;
+}
+
+bool State::completeTable()
+{
+	State *s = new State("Актуально");
+
+	Database::open();
+	bool succeeded = s->insertIntoDatabase();
+	Database::close();
+	return succeeded;
+}
