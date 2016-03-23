ed1a0f506de47c061dd6e36f8102924ee462c145
Anna Iliyina <anna.iliyina@gmail.com>
2016-03-23 03:20:04 +0300

    Added files via upload

---
+++ search_module/SM_Session.cpp
@@ -0,0 +1,28 @@
+﻿#include "SM_Session.h"
+#include "Database.h"
+#include "QStatusBar"
+
+/*!
+\file
+\brief 
+*/
+
+SM_Session::SM_Session()
+{
+	
+}
+
+SM_Session::~SM_Session()
+{
+}
+
+
+/*!
+Отправляет сигнал основному окну с целью поменять 
+сообщение панели QStatusBar
+\param const QString &text - текст нового сообщения
+*/
+//void SM_Session::newMessage(const QString &text)
+//{
+//	emit newStatusbarText(text);
+//}
---
+++ search_module/SM_Session.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QString>
+#include <QObject>
+#include <QDebug>
+#include <QUrl>
+#include <QDateTime>
+
+/*!
+\file
+\brief 
+*/
+
+/*! 
+ *  \brief     Класс SM_Session (Search Module session)
+ *  \details  Служит для поиска информации о ГПИ в интернете.
+ *  \author    Ильина А.
+ *  \date      март 2016
+ */
+
+class SM_Session /*: public QObject {
+	Q_OBJECT*/
+{
+public:
+	SM_Session();
+	~SM_Session();
+//private:
+//	void newMessage(const QString &text);
+//signals:
+//	void newStatusbarText(const QString &text);
+};
