342ecfd1c63905bb79f37b25ec505122b57cd51a
Anna Iliyina <anna.iliyina@gmail.com>
2016-03-25 17:39:46 +0300

    Added files via upload

---
+++ ["view"]/MainWindow.h
@@ -0,0 +1,28 @@
+﻿#pragma once 
+#include <QMainWindow>
+#include <QWidget>
+#include <QString>
+#include "LoginDialog.h"
+/*
+\file
+*/
+
+/*!
+\brief Класс MainWindow
+\details Описывает конфигурацию основного окна приложения 
+\author 
+\date март 2016г.
+*/
+class MainWindow : public QMainWindow {
+	Q_OBJECT
+public:
+	explicit MainWindow(QMainWindow *parent = 0);
+	~MainWindow();
+private:
+	void configure();
+private slots:
+	void showMW();
+	void showMessage(const QString &message);
+};
+
+
