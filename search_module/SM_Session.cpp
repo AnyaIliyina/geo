18bd14bbdf37145b18eb36d84f59377c61258342
Anna Iliyina <anna.iliyina@gmail.com>
2016-03-25 17:57:09 +0300

    Update SM_Session.cpp

--- search_module/SM_Session.cpp
+++ search_module/SM_Session.cpp
@@ -1,6 +1,8 @@
-﻿#include "SM_Session.h"
+#include "SM_Session.h"
 #include "Database.h"
 #include "QStatusBar"
+#include "State.h"
+#include "Session.h"
 
 /*!
 \file
@@ -16,13 +18,23 @@ SM_Session::~SM_Session()
 {
 }
 
+/*!
+Начинает сессию поиска
+*/
+void SM_Session::start()
+{
+	Session *s = new Session(1, QDateTime::currentDateTime());
+	s->insertIntoDatabase();
+	emit newMessage(State::coded("Модуль поиска начал работу..."));
+}
+
 
 /*!
 Отправляет сигнал основному окну с целью поменять 
 сообщение панели QStatusBar
 \param const QString &text - текст нового сообщения
 */
-//void SM_Session::newMessage(const QString &text)
-//{
-//	emit newStatusbarText(text);
-//}
+void SM_Session::newMessage(const QString &text)
+{
+	emit newStatusbarText(text);
+}
