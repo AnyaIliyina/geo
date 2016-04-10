749e90fc09a111f998560408166b197e4f5fe732
mardj258 <begin258@mail.ru>
2016-04-10 13:07:09 +0300

    Update Status.h

--- core/Status.h
+++ core/Status.h
@@ -22,13 +22,45 @@ private:
 
 public:
 	QString status_name();
+		
+	/*!
+	Конструирует объект класса Status из параметров
+	\param const QString status_name - название создаваемого статуса
+	*/
 	Status(QString status_name);
+	
+	/*!
+	Конструирует объект класса Status из данных в базе
+	\param int id - id статуса в базе
+	*/
 	Status(int id);
+	
 	~Status();
+	
+	/*!
+	Возвращает id статуса
+	\return int status_id - id статуса
+	*/
 	int status_id();
+	
+	/*!
+	Записывает информацию о статусе в базу данных.
+	\return true - если запись в БД успешно добавлена
+	*/
 	bool insertIntoDatabase();
+	
+	/*!
+	Создает таблицу "statuses" в базе данных.
+	\return true - если таблица успешно создана
+	*/
 	static bool createTable();
+
+	/*!
+	Заполняет таблицу "statuses" в БД начальными значениями.
+	\return true - если таблица успешно заполнена
+	*/
 	static bool completeTable();
+	
 	QString static coded(QByteArray encodedStr);
 };
 
