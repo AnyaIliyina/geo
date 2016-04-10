d919120d43fb986ab2912f88d1759830fecaed0b
mardj258 <begin258@mail.ru>
2016-04-10 13:13:52 +0300

    Update User.h

--- core/User.h
+++ core/User.h
@@ -24,12 +24,45 @@ private:
 
 public:
 	QString login();
+	
+	/*!
+	Конструирует объект класса User из параметров
+	\param int type_id - идентификатор типа пользователя 
+	\param QString login - логин пользователя
+	\param  QString password - пароль пользователя
+	*/
 	User(int type_id, QString login, QString password);
+	
+	/*!
+	Конструирует объект класса User из данных в базе
+	\param int id - id пользователя в базе
+	*/
 	User(int id);
+	
 	~User();
+	
+	/*!
+	Возвращает id пользователя
+	\return int user_id - id пользователя в базе
+	*/
 	int user_id();
+	
+	/*!
+	Записывает информацию о пользователе в базу данных.
+	\return true - если запись в БД успешно добавлена
+	*/
 	bool insertIntoDatabase();
+	
+	/*!
+	Создает таблицу "users" в базе данных.
+	\return true - если таблица успешно создана
+	*/
 	static bool createTable();
+	
+	/*!
+	Заполняет таблицу "users" в БД начальными значениями.
+	\return true - если таблица успешно заполнена
+	*/
 	static bool completeTable();
 	
 };
