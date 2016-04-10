0e36f7831593226ffd77dda6cdd691409145a1aa
mardj258 <begin258@mail.ru>
2016-04-10 13:18:03 +0300

    Update Usertype.h

--- core/Usertype.h
+++ core/Usertype.h
@@ -22,13 +22,45 @@ private:
 
 public:
 	QString type_name();
+	
+	/*!
+	Конструирует объект класса Usertype из параметров
+	\param QString type_name - название создаваемого типа
+	*/
 	Usertype(QString type_name);
+	
+	/*!
+	Конструирует объект класса Usertype из данных в базе
+	\param int id - id статуса в базе
+	*/
 	Usertype(int id);
+	
 	~Usertype();
+	
+	/*!
+	Возвращает id типа пользователя
+	\return int type_id - id типа пользователя
+	*/
 	int type_id();
+	
+	/*!
+	Записывает информацию о типе пользователя в базу данных.
+	\return true - если запись в БД успешно добавлена
+	*/
 	bool insertIntoDatabase();
+	
+	/*!
+	Создает таблицу "usertypes" в базе данных.
+	\return true - если таблица успешно создана
+	*/
 	static bool createTable();
+	
+	/*!
+	Заполняет таблицу "usertypes" в БД начальными значениями.
+	\return true - если таблица успешно заполнена
+	*/
 	static bool completeTable();
+	
 	QString static coded(QByteArray encodedStr);
 };
 
