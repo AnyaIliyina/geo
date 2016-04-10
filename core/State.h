b1a85131c69ce9d00718c7d6b103ebe13b4b1fdc
mardj258 <begin258@mail.ru>
2016-04-10 12:59:42 +0300

    Update State.h

--- core/State.h
+++ core/State.h
@@ -22,12 +22,44 @@ private:
 
 public:
 	QString state_name();
+	
+	/*!
+	Конструирует объект класса State из параметров
+	\param const QString state_name - название создаваемого статуса
+	*/
 	State(QString state_name);
+	
+	/*!
+	Конструирует объект класса State из данных в базе
+	\param int id - id статуса в базе
+	*/
 	State(int id);
+	
 	~State();
+	
+	/*!
+	Возвращает id статуса
+	\return int state_id - id статуса
+	*/
 	int state_id();
+	
+	/*!
+	Записывает информацию о статусе в базу данных.
+	\return true - если запись в БД успешно добавлена
+	*/
 	bool insertIntoDatabase();
+	
+	/*!
+	Создает таблицу "states" в базе данных.
+	\return true - если таблица успешно создана
+	*/
 	static bool createTable();
+	
+	/*!
+	Заполняет таблицу "states" в БД начальными значениями.
+	\return true - если таблица успешно заполнена
+	*/
 	static bool completeTable();
+	
 	QString static coded(QByteArray encodedStr);
 };
