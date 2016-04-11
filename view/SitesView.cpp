200b6452fef6a0669563a0196cfbc2f90f9ac485
mardj258 <begin258@mail.ru>
2016-04-11 17:53:32 +0300

    Added files via upload

---
+++ view/SitesView.cpp
@@ -0,0 +1,46 @@
+﻿#include "SitesView.h"
+#include "ui_SitesView.h"
+#include "State.h"
+#include <QTableWidget>
+#include <QSqlQueryModel>
+#include "Database.h"
+/*!
+\file
+\brief Временный класс для вывода содержимого таблицы Sites
+*/
+
+SitesView::SitesView(QDockWidget *parent) :
+	QDockWidget(parent),
+	ui(new Ui::SitesView)
+{
+	ui->setupUi(this);
+	this->setupModel( QStringList() 
+		<< ("URL")
+		<< State::coded("Название сайта"));
+	this->createUi();
+}
+
+SitesView::~SitesView()
+{
+	delete ui;
+}
+
+void SitesView::setupModel( const QStringList & headers)
+{
+	Database::open();
+	model = new QSqlQueryModel(this);
+	model->setQuery("SELECT site_name, url FROM sites WHERE site_id in (select site_id from geodata_records WHERE place_name='Ekaterinburg')");
+	for (int i = 0, j = 0; i < model->columnCount(); i++, j++)
+	{
+		model->setHeaderData(i, Qt::Horizontal, headers[j]);
+	}
+
+}
+
+void SitesView::createUi()
+{
+	ui->tableView->setModel(model);
+	//ui->tableView->setColumnHidden(0, true);
+	
+	
+}
---
+++ view/SitesView.h
@@ -0,0 +1,27 @@
+﻿#include "ui_SitesView.h"
+#include <qapplication.h>
+#include <QMainWindow>
+#include <QSqlTableModel>
+
+
+namespace Ui {
+	class SitesView;
+}
+
+/*!
+*  \brief Выводит содержимое таблицы Sites (временно)
+*  \author Козырева О.
+*  \date   март 2016
+*/
+class  SitesView:public QDockWidget {
+	Q_OBJECT
+public:
+	explicit SitesView(QDockWidget *parent = 0);
+	~SitesView();
+private:
+	Ui::SitesView *ui;
+	QSqlQueryModel *model;
+private:
+	void setupModel(const QStringList &headers);
+	void createUi();
+};
