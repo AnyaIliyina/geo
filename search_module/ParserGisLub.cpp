b84af0872326a427dd62fd62d815601f060ba562
Anna Iliyina <anna.iliyina@gmail.com>
2016-04-14 10:49:08 +0300

    let's parse

---
+++ search_module/ParserGisLub.cpp
@@ -0,0 +1,26 @@
+﻿#include "ParserGisLub.h"
+#include <QDebug>
+
+/*!
+\file
+\brief 
+*/
+
+int ParserGisLub::parse()
+{
+	const QByteArray *ar = getReply(m_url);
+	if (*ar == "")
+		return PAGE_NOT_AVAILABLE;
+}
+
+
+ParserGisLub::ParserGisLub(int session_id)
+{
+	m_session_id = session_id;
+}
+
+
+
+ParserGisLub::~ParserGisLub()
+{
+}
