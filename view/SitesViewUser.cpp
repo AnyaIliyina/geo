528c18c8b6640a631f0dc7dfa8c0efab9d4d365d
mardj258 <begin258@mail.ru>
2016-04-16 14:09:27 +0300

    Added files via upload

---
+++ view/SitesViewUser.cpp
@@ -0,0 +1,82 @@
+﻿#include "SitesViewUser.h"
+#include "ui_SitesViewUser.h"
+#include "State.h"
+#include <QTableWidget>
+#include <QSqlQueryModel>
+#include "Database.h"
+#include "NewDockWidget.h"
+#include "NewRecordUser.h"
+/*!
+\file
+\brief
+*/
+
+SitesViewUser::SitesViewUser(QDockWidget *parent) :QDockWidget(parent), ui(new Ui::SitesViewUser)
+{
+	ui->setupUi(this);
+	QObject::connect(ui->btnSearch, SIGNAL(clicked()), this, SLOT(WhereQueryPart()));
+	NewRecordUser *ndw = new NewRecordUser();
+	QObject::connect(ui->btnNew, SIGNAL(clicked()), ndw, SLOT(showNDW()));
+}
+
+SitesViewUser::~SitesViewUser()
+{
+	delete ui;
+}
+
+QString SitesViewUser::ParseWhereArgs(QList<QString>& args)
+{
+	if (args.length() == 0)
+	{
+		return QString();
+	}
+	QString qry = " WHERE ";
+	for (int i = 0;i < args.length();i++)
+	{
+		qry += args[i];
+		if (i != (args.length() - 1))
+		{
+			qry += " AND ";
+		}
+	}
+	return qry;
+}
+
+void SitesViewUser::WhereQueryPart()
+{
+	QList<QString> whereArgs;
+	if (!ui->textGeo->text().isEmpty())
+	{
+		whereArgs.push_back(QString("(site_id IN (SELECT site_id FROM geodata_records WHERE place_name='%1'))").arg(ui->textGeo->text()));
+	}
+	if (!ui->textURL->text().isEmpty())
+	{
+		whereArgs.push_back(QString("(site_name='%1')").arg(ui->textURL->text()));
+	}
+	if (!ui->comboBox->currentText().isEmpty())
+	{
+		whereArgs.push_back(QString("(site_id IN (SELECT site_id FROM geodata_records WHERE format_id in (SELECT format_id FROM formats WHERE format_name='%1')))").arg(ui->comboBox->currentText()));
+	}
+	QString whereQryPart = ParseWhereArgs(whereArgs);
+	setupModel(whereQryPart);
+}
+
+void SitesViewUser::setupModel(QString& whereQryPart)
+{
+	QSqlDatabase db = Database::database();
+	model = new QSqlQueryModel(this);
+	model->setQuery(QString("SELECT site_name, url FROM sites %1").arg(whereQryPart), db);
+	createTable();
+
+}
+
+
+
+void SitesViewUser::createTable()
+{
+	ui->tableView->setModel(model);
+	ui->tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+	ui->tableView->setSelectionMode(QAbstractItemView::SingleSelection);
+
+}
+
---
+++ view/SitesViewUser.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "ui_SitesViewUser.h"
+#include <qapplication.h>
+#include <QMainWindow>
+#include <QSqlTableModel>
+
+namespace Ui {
+	class SitesViewUser;
+}
+
+/*!
+*  \brief Выводит содержимое таблицы Sites (временно)
+*  \author Козырева О.
+*  \date   март 2016
+*/
+class  SitesViewUser :public QDockWidget {
+	Q_OBJECT
+public:
+	explicit SitesViewUser(QDockWidget *parent = 0);
+	~SitesViewUser();
+
+	/*!
+	Метод для создания строки запроса
+	\param QList<QString> args - список, содержащий строки, для составления запроса
+	\return QString qry - строка запроса к базе
+	*/
+	QString ParseWhereArgs(QList<QString>& args);
+
+	/*!
+	\param QString whereQryPart- строка запроса к базе
+	*/
+	void setupModel(QString& whereQryPart);
+private:
+	Ui::SitesViewUser *ui;
+	QSqlQueryModel *model;
+private:
+	void createTable();
+
+	public slots:
+	/*!
+	Слот для выполнения поиска по введенным данным
+	*/
+	void WhereQueryPart();
+
+};
