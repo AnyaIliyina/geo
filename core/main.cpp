b5d79109f2ca64d5fcac10ad01330572a8df5715
Anna Iliyina <anna.iliyina@gmail.com>
2016-04-25 12:07:19 +0300

    Added files via upload

---
+++ core/main.cpp
@@ -0,0 +1,19 @@
+#include <QApplication>
+#include <qDebug>
+#include "Database.h"
+#include "ParserGeofabrik.h"
+#include "MainWindow.h"
+
+/*!
+\file
+\brief Enterance point
+*/
+int main(int argc, char* argv[])
+{	
+	QApplication app(argc, argv);
+	Database::restore();
+	ParserGeofabrik *p = new ParserGeofabrik();
+	p->parse(1, 1);
+	MainWindow *w = new MainWindow();
+	return app.exec();
+};
