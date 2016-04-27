c7b0a3fe200331aa4a5404f73641ce56a7621cb9
Anna Iliyina <anna.iliyina@gmail.com>
2016-04-27 18:43:24 +0300

    недоТаблица

--- core/TableModel.cpp
+++ core/TableModel.cpp
@@ -1,10 +1,31 @@
 #include "TableModel.h"
 #include "State.h"
+#include "Database.h"
+#include <QSqlRecord>
+#include <QSqlTableModel>
 
 TableModel::TableModel(QObject *parent) :QAbstractTableModel(parent)
 {
+	QSqlDatabase db = Database::database();
+	QSqlTableModel model(nullptr, db);
+	model.setTable("geodata_records");
+	/*const QString filter = QString("record_id == %1").arg(id);
+	model.setFilter(filter);*/
+	model.select();
+	for (int i = 0; i < model.rowCount(); i++)
+	{
+		int row = m_geo.count();
+		beginInsertRows(QModelIndex(), row, row);
+		GeoData g;
 
+		g[PLACE_NAME] = model.record(i).value("place_name").toString();
+		g[FORMAT_NAME] = model.record(0).value("format_id").toInt();
+		
+		m_geo << g;
+		endInsertRows();
+	}
 }
+
 
 int TableModel::rowCount(const QModelIndex & parent) const
 {
