b8f4d4942da725016052a9deb44e90ddc7b0a332
mardj258 <begin258@mail.ru>
2016-04-27 13:27:37 +0300

    херь какая-то

---
+++ debug/moc_ViewWindow.cpp
@@ -0,0 +1,134 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'ViewWindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../view/ViewWindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'ViewWindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_ViewWindow_t {
+    QByteArrayData data[4];
+    char stringdata0[44];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ViewWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ViewWindow_t qt_meta_stringdata_ViewWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "ViewWindow"
+QT_MOC_LITERAL(1, 11, 13), // "signalDeleted"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 17) // "slotDeleteMessage"
+
+    },
+    "ViewWindow\0signalDeleted\0\0slotDeleteMessage"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ViewWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    0,   25,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void ViewWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        ViewWindow *_t = static_cast<ViewWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->signalDeleted(); break;
+        case 1: _t->slotDeleteMessage(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (ViewWindow::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&ViewWindow::signalDeleted)) {
+                *result = 0;
+            }
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject ViewWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_ViewWindow.data,
+      qt_meta_data_ViewWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *ViewWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ViewWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_ViewWindow.stringdata0))
+        return static_cast<void*>(const_cast< ViewWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ViewWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void ViewWindow::signalDeleted()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
+}
+QT_END_MOC_NAMESPACE
---
+++ ui_ViewWindow.h
@@ -0,0 +1,126 @@
+/********************************************************************************
+** Form generated from reading UI file 'ViewWindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_VIEWWINDOW_H
+#define UI_VIEWWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_ViewWindow
+{
+public:
+    QAction *action_New;
+    QAction *action_Delete;
+    QAction *action_Edit;
+    QAction *action_Yes;
+    QAction *action_No;
+    QWidget *centralwidget;
+    QGridLayout *gridLayout;
+    QTableView *tableView;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *ViewWindow)
+    {
+        if (ViewWindow->objectName().isEmpty())
+            ViewWindow->setObjectName(QStringLiteral("ViewWindow"));
+        ViewWindow->resize(670, 389);
+        action_New = new QAction(ViewWindow);
+        action_New->setObjectName(QStringLiteral("action_New"));
+        QIcon icon;
+        icon.addFile(QStringLiteral(":/plus.png"), QSize(), QIcon::Normal, QIcon::Off);
+        action_New->setIcon(icon);
+        action_Delete = new QAction(ViewWindow);
+        action_Delete->setObjectName(QStringLiteral("action_Delete"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral(":/trash.png"), QSize(), QIcon::Normal, QIcon::Off);
+        action_Delete->setIcon(icon1);
+        action_Edit = new QAction(ViewWindow);
+        action_Edit->setObjectName(QStringLiteral("action_Edit"));
+        QIcon icon2;
+        icon2.addFile(QStringLiteral(":/note.png"), QSize(), QIcon::Normal, QIcon::Off);
+        action_Edit->setIcon(icon2);
+        action_Yes = new QAction(ViewWindow);
+        action_Yes->setObjectName(QStringLiteral("action_Yes"));
+        QIcon icon3;
+        icon3.addFile(QStringLiteral(":/check.png"), QSize(), QIcon::Normal, QIcon::Off);
+        action_Yes->setIcon(icon3);
+        action_No = new QAction(ViewWindow);
+        action_No->setObjectName(QStringLiteral("action_No"));
+        QIcon icon4;
+        icon4.addFile(QStringLiteral(":/close.png"), QSize(), QIcon::Normal, QIcon::Off);
+        action_No->setIcon(icon4);
+        centralwidget = new QWidget(ViewWindow);
+        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        gridLayout = new QGridLayout(centralwidget);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName(QStringLiteral("tableView"));
+        tableView->setMinimumSize(QSize(550, 200));
+        tableView->setStyleSheet(QLatin1String("QTableView{\n"
+"border-width:2px\n"
+"}"));
+
+        gridLayout->addWidget(tableView, 0, 0, 1, 1);
+
+        ViewWindow->setCentralWidget(centralwidget);
+        toolBar = new QToolBar(ViewWindow);
+        toolBar->setObjectName(QStringLiteral("toolBar"));
+        toolBar->setMovable(false);
+        ViewWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        toolBar->addAction(action_New);
+        toolBar->addAction(action_Delete);
+        toolBar->addSeparator();
+        toolBar->addAction(action_Edit);
+        toolBar->addSeparator();
+        toolBar->addAction(action_Yes);
+        toolBar->addAction(action_No);
+
+        retranslateUi(ViewWindow);
+
+        QMetaObject::connectSlotsByName(ViewWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *ViewWindow)
+    {
+        ViewWindow->setWindowTitle(QApplication::translate("ViewWindow", "MainWindow", 0));
+#ifndef QT_NO_TOOLTIP
+        ViewWindow->setToolTip(QString());
+#endif // QT_NO_TOOLTIP
+        action_New->setText(QApplication::translate("ViewWindow", "&New", 0));
+        action_Delete->setText(QApplication::translate("ViewWindow", "&Delete", 0));
+        action_Edit->setText(QApplication::translate("ViewWindow", "&Edit", 0));
+        action_Yes->setText(QApplication::translate("ViewWindow", "&Yes", 0));
+        action_No->setText(QApplication::translate("ViewWindow", "&No", 0));
+        toolBar->setWindowTitle(QApplication::translate("ViewWindow", "toolBar", 0));
+#ifndef QT_NO_TOOLTIP
+        toolBar->setToolTip(QString());
+#endif // QT_NO_TOOLTIP
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class ViewWindow: public Ui_ViewWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_VIEWWINDOW_H
